70e47cc13115a320f602d9e7b27006ca30282e9c
Loic <loicgevrey@gmail.com>
2015-10-04 18:58:49 +0200

    Improve check ip

--- core/class/dyndns.class.php
+++ core/class/dyndns.class.php
@@ -25,10 +25,15 @@ class dyndns extends eqLogic {
 	/*     * ***********************Methode static*************************** */
 
 	public static function getExternalIP() {
-		$request_http = new com_http('http://checkip.dyndns.com/');
-		$externalContent = $request_http->exec();
-		preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
-		return $m[1];
+		try {
+			$request_http = new com_http('http://checkip.dyndns.com/');
+			$externalContent = $request_http->exec(2, 0);
+			preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
+			return $m[1];
+		} catch (Exception $e) {
+			$request_http = new com_http('http://myip.dnsomatic.com/');
+			return $request_http->exec(2, 0);
+		}
 	}
 
 	public static function cron15($_eqLogic_id = null, $_force = false) {
