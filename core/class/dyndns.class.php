d975eca3762e7ade8834b618bd052a55b5a25338
Loïc <loic@jeedom.com>
2017-02-06 11:20:11 +0100

    Update dyndns.class.php

--- core/class/dyndns.class.php
+++ core/class/dyndns.class.php
@@ -105,8 +105,8 @@ class dyndns extends eqLogic {
 				}
 				break;
 			case 'noipcom':
-				$url = 'https://' . $this->getConfiguration('username') . ':' . $this->getConfiguration('password') . '@dynupdate.no-ip.com/nic/update?hostname=' . $this->getConfiguration('hostname') . '&myip=' . $ip;
-				$request_http = new com_http($url);
+				$url = 'https://' . '@dynupdate.no-ip.com/nic/update?hostname=' . $this->getConfiguration('hostname') . '&myip=' . $ip;
+				$request_http = new com_http($url,$this->getConfiguration('username'),$this->getConfiguration('password'));           	
 				$request_http->setUserAgent('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.12) Gecko/20070508 Firefox/1.5.0.12');
 				$result = $request_http->exec();
 				if (strpos($result, 'good') === false || strpos($result, 'nochg') === false) {
@@ -122,7 +122,7 @@ class dyndns extends eqLogic {
 					throw new Exception(__('Erreur de mise à jour de ovh.com : ', __FILE__) . $result);
 				}
 				break;
-      case 'duckdns':
+      			case 'duckdns':
 				$url = 'https://www.duckdns.org/update?domains=' . $this->getConfiguration('hostname') . '&token=' . $this->getConfiguration('token') . '&ip=' . $ip;
 				$request_http = new com_http($url);
 				$request_http->setUserAgent('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.12) Gecko/20070508 Firefox/1.5.0.12');
