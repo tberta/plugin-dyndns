37bb7348e32a9b03dce263643fe62de24340435e
Loïc <loic@jeedom.com>
2020-01-13 10:09:20 +0100

    Update dyndns.php

--- desktop/php/dyndns.php
+++ desktop/php/dyndns.php
@@ -35,7 +35,7 @@ foreach ($eqLogics as $eqLogic) {
 
 </div>
 
-<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="display: none;">
+<div class="col-lg-12 eqLogic" style="display:none;">
 	<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
 				<a class="btn btn-default btn-sm eqLogicAction roundedLeft" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
